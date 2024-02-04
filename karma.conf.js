7b70d184cf857b45931431259810c96b3e71172f
csmacharla <csmacharla@gmail.com>
2024-02-03 21:47:41 -0700

    CHROME HEADLESS ON JENKINS

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
-process.env.CHROME_BIN = require('puppeteer').executablePath()
-
+const os = require('os');
+const chromeHeadlessSupported = os.platform() !== 'win32' || Number((os.release().match(/^(\d+)/) || ['0', '0'])[1]) >= 10;
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -7,31 +7,38 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
+      require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
+      dir: require('path').join(__dirname, '../../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
-        port: 9876,
+    reporters: ['mocha','coverage'],
+    coverageReporter: {
+      reports: ['html', 'lcovonly'],
+      type: 'lcov'
+    },
+    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-
-    browsers: ['ChromeHeadlessNSandbox'],
+    browserNoActivityTimeout: 30000,
+    browsers: [
+      chromeHeadlessSupported ? 'ChromeHeadless' : 'Chrome'
+    ],
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+      ChromeHeadless: {
+          base: 'Chrome',
+          flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
       }
     },
-    singleRun: false
+    singleRun: true
   });
 };
